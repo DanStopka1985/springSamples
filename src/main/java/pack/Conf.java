abf145d631d78079ab584c08cea1813340e40f2d
DanStopka1985 <DanStopka1985@gmail.com>
2015-12-07 20:49:13 +0300

    [nf] thymeleaf simle added

--- src/main/java/pack/Conf.java
+++ src/main/java/pack/Conf.java
@@ -3,6 +3,11 @@ package pack;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 /**
  * Created by RTLabs on 07.12.2015.
@@ -10,8 +15,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 public class Conf {
+
     @Bean
-    Figure figure(){
+    public TemplateResolver templateResolver(){
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver() ;
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+
+        return viewResolver;
+    }
+
+    @Bean
+    Figure figure(  ){
         Circle c = new Circle();
         c.setR(10);
         return c;
--- src/main/java/pack/Run.java
+++ src/main/java/pack/Run.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,18 +14,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by RTLabs on 07.12.2015.
  */
 @Controller
-
+//@RequestMapping(value = "/", method = RequestMethod.GET)
 public class Run {
     @Autowired
     Figure figure;
 
-    @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
-    @ResponseBody
-    public String a(){
-        String s = "Perimeter: " + String.valueOf(figure.getPerimeter()) + "<br>" +
-                ("Space: " + figure.getSpace());
+//    @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+//    @ResponseBody
+//    public String a(){
+//        String s = "Perimeter: " + String.valueOf(figure.getPerimeter()) + "<br>" +
+//                ("Space: " + figure.getSpace());
+//
+//        return s;
+//    }
 
-        return s;
+//    @RequestMapping(value = "/index", method = RequestMethod.GET)
+
+    @RequestMapping(value = "index", method = RequestMethod.GET)
+    public String b(Model model){
+        model.addAttribute("title", "100100");
+
+        return "index";
     }
 
 }
