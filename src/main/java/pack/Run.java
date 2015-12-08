d1ce608644774e02bbcd4ce9c3ba6dcfb7065cd3
DanStopka1985 <DanStopka1985@gmail.com>
2015-12-08 14:47:55 +0300

    [nf]

--- src/main/java/pack/Run.java
+++ src/main/java/pack/Run.java
@@ -1,14 +1,21 @@
 package pack;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pack.Entities.Author;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by RTLabs on 07.12.2015.
@@ -18,6 +25,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class Run {
     @Autowired
     Figure figure;
+
+    @Autowired
+    private SessionFactory sessionFactory;
 
 //    @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
 //    @ResponseBody
@@ -31,7 +41,19 @@ public class Run {
 //    @RequestMapping(value = "/index", method = RequestMethod.GET)
 
     @RequestMapping(value = "index", method = RequestMethod.GET)
+    @Transactional(readOnly=true)
     public String b(Model model){
+
+        //@SuppressWarnings("unchecked")
+
+        //System.out.println(sessionFactory.getStatistics().getCollectionRoleNames());
+
+        Session s = sessionFactory.getCurrentSession();
+
+//        List authors = sessionFactory.getCurrentSession()
+//                .createQuery("from Author ")
+//                .list();
+
         model.addAttribute("title", "100100");
 
         return "index";
