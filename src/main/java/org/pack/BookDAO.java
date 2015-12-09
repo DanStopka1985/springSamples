dd153654de6f928eaf2796047d3edb57d5028414
DanStopka1985 <DanStopka1985@gmail.com>
2015-12-09 11:06:52 +0300

    [nf]

---
+++ src/main/java/org/pack/BookDAO.java
@@ -0,0 +1,25 @@
+package org.pack;
+
+import org.entities.Book;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+public class BookDAO {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Transactional(readOnly=true)
+    public List getAll(){
+
+        return sessionFactory.getCurrentSession()
+                .createQuery("select a from Book a")
+                .list();
+    }
+
+}
