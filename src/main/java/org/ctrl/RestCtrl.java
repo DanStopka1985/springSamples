c5178e8d5f6d42c69cb09763b21663ecd270990f
DanStopka1985 <DanStopka1985@gmail.com>
2015-12-09 19:23:28 +0300

    2

--- src/main/java/org/ctrl/RestCtrl.java
+++ src/main/java/org/ctrl/RestCtrl.java
@@ -1,11 +1,9 @@
 package org.ctrl;
 import org.dao.BookDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.entities.Book;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.temp.Figure;
 
@@ -29,6 +27,11 @@ public class RestCtrl {
     @RequestMapping(value = "books", method = RequestMethod.GET)
     public List<Book> getBooks(){
         return bookDAO.getAll();
+    }
+
+    @RequestMapping(value = "book/{id}", method = RequestMethod.GET)
+    public Book getBookById(@PathVariable int id){
+        return bookDAO.getById(id);
     }
 
     @RequestMapping(value = "x", method = RequestMethod.GET)
--- src/main/java/org/dao/BookDAO.java
+++ src/main/java/org/dao/BookDAO.java
@@ -1,5 +1,6 @@
 package org.dao;
 
+import org.entities.Book;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,12 +14,21 @@ public class BookDAO {
     @Autowired
     private SessionFactory sessionFactory;
 
-    @Transactional(readOnly=true)
+    @Transactional(readOnly = true)
     public List getAll(){
 
         return sessionFactory.getCurrentSession()
-                .createQuery("select a from Book a")
+                .createQuery("select b from Book b")
                 .list();
+    }
+    @Transactional(readOnly = true)
+    public Book getById(int id){
+        List<Book> books = sessionFactory.getCurrentSession()
+                .createQuery("select b from Book b where b.id = :id")
+                .setParameter("id", id).list();
+
+        if (books.size() > 0) return books.get(0);
+        else return null;
     }
 
 }
